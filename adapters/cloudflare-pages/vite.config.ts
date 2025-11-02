975188c204fd306b6c570034c9ba51c9bdff78c6
WebHoga Digital Services <contact@webhoga.com>
2025-11-02 15:57:21 +0530

    Initial commit

---
+++ adapters/cloudflare-pages/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { cloudflarePagesAdapter } from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
+
+export default defineConfig(() => {
+    return {
+        plugins: [
+            qwikCity(),
+            qwikVite(),
+            tsconfigPaths(),
+            cloudflarePagesAdapter()
+        ],
+    };
+});
