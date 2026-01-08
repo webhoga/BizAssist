509778d88381fbf62c50e5232a21e04a8ba0b80f
webhoga <contact@webhoga.com>
2026-01-08 15:53:59 +0530

    Initial commit

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -1,23 +1,8 @@
-/*
- * WHAT IS THIS FILE?
- *
- * It's the entry point for Cloudflare Pages when building for production.
- *
- * Learn more about the Cloudflare Pages integration here:
- * - https://qwik.dev/docs/deployments/cloudflare-pages/
- *
- */
-import {
-  createQwikCity,
-  type PlatformCloudflarePages,
-} from "@builder.io/qwik-city/middleware/cloudflare-pages";
-import qwikCityPlan from "@qwik-city-plan";
-import render from "./entry.ssr";
+import { createQwikCity } from '@builder.io/qwik-city/middleware/cloudflare-pages';
+import qwikCityPlan from '@qwik-city-plan';
+import { manifest } from '@qwik-client-manifest';
+import render from './entry.ssr';
 
-declare global {
-  type QwikCityPlatform = PlatformCloudflarePages;
-}
+const onRequest = createQwikCity({ render, qwikCityPlan, manifest });
 
-const fetch = createQwikCity({ render, qwikCityPlan });
-
-export { fetch };
+export { onRequest };
