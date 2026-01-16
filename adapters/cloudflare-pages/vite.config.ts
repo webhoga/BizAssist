074c92f3fd9b252b2586c693bcb7b9539566c30a
webhoga <contact@webhoga.com>
2026-01-16 07:45:38 +0530

    Initial commit

--- adapters/cloudflare-pages/vite.config.ts
+++ adapters/cloudflare-pages/vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import { cloudflarePagesAdapter } from "@builder.io/qwik-city/adapters/cloudflare-pages/vite";
+import baseConfig from "../../vite.config";
 
-export default defineConfig(() => {
+export default defineConfig((env) => {
+    // Get the base config (plugins like qwikVite, etc)
+    const base = typeof baseConfig === "function" ? baseConfig(env) : baseConfig;
+
     return {
+        ...base,
         plugins: [
-            qwikCity(),
-            qwikVite(),
-            tsconfigPaths(),
+            ...(base.plugins || []),
+            cloudflarePagesAdapter(),
         ],
         build: {
             ssr: true,
             rollupOptions: {
-                input: ['src/entry.cloudflare-pages.tsx'],
+                input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
             },
         },
     };
